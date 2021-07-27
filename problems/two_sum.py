9661e6992f700c9bbf872b04ec4fce643eb3a824
Aditya Bose <13054902+adbose@users.noreply.github.com>
2021-07-27 20:06:45 +0530

    Add two sum problem solution

---
+++ problems/two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # using a dict to store the items iterated/visited/seen
+        seen = {}
+        for i in range(len(nums)):
+            if target - nums[i] in seen.keys():
+                return ([seen[target-nums[i]], i])
+            seen[nums[i]] = i
